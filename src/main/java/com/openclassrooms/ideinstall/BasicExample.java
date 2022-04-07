5ecec6de7e508438a62250b89b3d25512bbdefde
psdiaz <psdiaz@nokinlab.com>
2022-04-08 01:09:21 +0200

    Second fix

--- src/main/java/com/openclassrooms/ideinstall/BasicExample.java
+++ src/main/java/com/openclassrooms/ideinstall/BasicExample.java
@@ -3,7 +3,6 @@ package com.openclassrooms.ideinstall;
 public class BasicExample {
 
 	public static void main(String[] args) {
-		final int index;
 		int i;
 		for (i = 0; i < 3; i++) {
 			System.out.println("Hello bugs !");
